b1f24cb9aea4851092ea6fe378dde1293ccf9ad6
Peter Marreck <lumbergh@gmail.com>
2022-10-14 16:25:03 -0400

    flake files not working yet, just checking in in interim

--- system76_thelio_nixos/flake.nix
+++ system76_thelio_nixos/flake.nix
@@ -1,15 +1,16 @@
 {
   inputs = {
-    unstable.url = github:NixOS/nixpkgs-channels/nixos-unstable;
-    stable.url = github:NixOS/nixpkgs-channels/nixos-22.05;
-    trunk.url = "github:NixOS/nixpkgs";
-    nixpkgs.url = github:NixOS/nixpkgs-channels/nixos-unstable;
-    home-manager.url = github:nix-community/home-manager;
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-hardware.url = github:NixOS/nixos-hardware/master;
+    unstable.url = github:NixOS/nixpkgs?ref=nixos-unstable ;
+    stable.url = github:NixOS/nixpkgs?ref=nixos-22.05 ;
+    trunk.url = github:NixOS/nixpkgs ;
+    nixpkgs.url = github:NixOS/nixpkgs?ref=nixos-unstable ; # How do I just refer to one of the above instead?
+    home-manager.url = github:nix-community/home-manager ;
+    home-manager.inputs.nixpkgs.follows = "nixpkgs" ;
+    nixos-hardware.url = github:NixOS/nixos-hardware/master ;
+    flake-utils.url = github:numtide/flake-utils ;
   };
   
-  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, flake-utils, ... }: {
     overlays = {
       # Inject 'unstable', 'stable' and 'trunk' into the overridden package set, so that
       # the following overlays may access them (along with any system configs
